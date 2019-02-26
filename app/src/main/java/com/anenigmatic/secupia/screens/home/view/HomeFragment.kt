52294284201239f967aca07104abc014cdbf0dbc
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-02-26 15:03:16 +0530

    Make Fragment for home screen

---
+++ app/src/main/java/com/anenigmatic/secupia/screens/home/view/HomeFragment.kt
@@ -0,0 +1,66 @@
+package com.anenigmatic.secupia.screens.home.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.anenigmatic.secupia.R
+import com.anenigmatic.secupia.screens.home.core.HomeViewModel
+import com.anenigmatic.secupia.screens.home.core.HomeViewModelFactory
+import com.anenigmatic.secupia.screens.home.core.UiOrder
+import kotlinx.android.synthetic.main.fra_home.view.*
+
+class HomeFragment : Fragment() {
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this, HomeViewModelFactory())[HomeViewModel::class.java]
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootPOV = inflater.inflate(R.layout.fra_home, container, false)
+
+        viewModel.checkLoginStatus()
+
+        rootPOV.logoutBTN.setOnClickListener {
+            viewModel.logout()
+        }
+
+        viewModel.orderData.observe(viewLifecycleOwner, Observer { order ->
+            when(order) {
+                is UiOrder.ShowLoadingState -> showLoadingState()
+                is UiOrder.ShowWorkingState -> showWorkingState()
+                is UiOrder.MoveToLogin      -> {
+
+                }
+            }
+        })
+
+        viewModel.toastData.observe(viewLifecycleOwner, Observer { toast ->
+            if(toast != null) {
+                Toast.makeText(context, toast, Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        return rootPOV
+    }
+
+
+    private fun showLoadingState() {
+        view?.let { view ->
+            view.loaderPBR.visibility = View.VISIBLE
+            view.contentSCL.visibility = View.GONE
+        }
+    }
+
+    private fun showWorkingState() {
+        view?.let { view ->
+            view.loaderPBR.visibility = View.GONE
+            view.contentSCL.visibility = View.VISIBLE
+        }
+    }
+}
