bc1653fd91e3a5a369872d8f37303d4fdf069327
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-02-26 18:49:34 +0530

    Make ViewModelFactory and setup di for vehicle logs screen

--- app/src/main/java/com/anenigmatic/secupia/di/vehicle/VehicleComponent.kt
+++ app/src/main/java/com/anenigmatic/secupia/di/vehicle/VehicleComponent.kt
@@ -1,10 +1,13 @@
 package com.anenigmatic.secupia.di.vehicle
 
 import com.anenigmatic.secupia.screens.vehicle.core.VehicleInfoViewModelFactory
+import com.anenigmatic.secupia.screens.vehicle.core.VehicleLogsViewModelFactory
 import dagger.Subcomponent
 
 @Subcomponent(modules = [])
 interface VehicleComponent {
 
     fun inject(factory: VehicleInfoViewModelFactory)
+
+    fun inject(factory: VehicleLogsViewModelFactory)
 }
