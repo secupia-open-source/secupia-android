0cc41e8776f6f788b5694b27dc8872dad166c33d
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-02-26 15:43:22 +0530

    Make status bar transparent and enable immersive mode

--- app/src/main/java/com/anenigmatic/secupia/screens/shared/view/MainActivity.kt
+++ app/src/main/java/com/anenigmatic/secupia/screens/shared/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anenigmatic.secupia.screens.shared.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.anenigmatic.secupia.R
 import com.anenigmatic.secupia.screens.home.view.HomeFragment
 
@@ -17,5 +18,18 @@ class MainActivity : AppCompatActivity() {
                 .commit()
 
         }
+    }
+
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE
     }
 }
